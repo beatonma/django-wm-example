d28176c746f8e6035466e928704fbf81e2b58206
Michael Beaton <beatonma@gmail.com>
2019-04-07 19:18:05 +0000

    Added webmention inline to ArticleAdmin

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
 from .models import MyMentionableArticle
+from mentions.models.webmention import Webmention
 
+class WebmentionInline(GenericTabularInline):
+    model = Webmention
+    readonly_fields = ['target_url', 'source_url', 'quote', 'hcard', 'sent_by', ]
+    can_delete = False
 
 @admin.register(MyMentionableArticle)
 class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    inlines = [
+        WebmentionInline,
+    ]
