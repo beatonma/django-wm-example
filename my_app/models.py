7d6cfdcd8cb6e0a916b167bf6e83f1fc2ef070cc
Michael Beaton <beatonma@gmail.com>
2019-04-08 21:20:31 +0000

    Added untested TemporaryMention implementation. Added __str__ implementation to MyMentionableArticle and reordered summary/main_content fields

--- my_app/models.py
+++ my_app/models.py
@@ -1,15 +1,20 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 from django.urls import reverse
 
 from mentions.models.mixins.mentionable import MentionableMixin
+from mentions.models.webmention import Webmention
+
+TEMPORARY_WEBMENTION_TIMEOUT = 60 * 15
 
 
 class MyMentionableArticle(MentionableMixin, models.Model):
     title = models.CharField(max_length=48)
     slug = models.SlugField(max_length=48)
 
-    main_content = models.TextField()
     summary = models.TextField()
+    main_content = models.TextField()
 
     def all_text(self) -> str:
         # We want to find potential outgoing webmention URLs
@@ -18,3 +23,15 @@ class MyMentionableArticle(MentionableMixin, models.Model):
 
     def get_absolute_url(self):
         return reverse('my_mentionable_article', args=[self.slug])
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class TemporaryMention(models.Model):
+    url = models.URLField()
+    submission_time = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def alive(self):
+        return datetime.now() - timedelta(seconds=TEMPORARY_WEBMENTION_TIMEOUT)
