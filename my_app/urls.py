46f590c355ad100d80401c31d616844de1ba165c
Michael Beaton <beatonma@gmail.com>
2019-04-08 21:19:26 +0000

    Server root url now points to webmention-tester

--- my_app/urls.py
+++ my_app/urls.py
@@ -13,9 +13,10 @@ log = logging.getLogger(__name__)
 urlpatterns = [
     # REQUIRED: Add `model_name` with `appname.modelname` to path kwargs!
     path(
-        'article/<slug:slug>',
+        '',
         MyMentionableArticleView.as_view(),
         kwargs={
+            'slug': 'webmention-tester',
             'model_name': 'my_app.MyMentionableArticle',
         },
         name='my_mentionable_article')
