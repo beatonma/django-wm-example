9f614c5a111d3709ae29a5c1d121219cda328f8e
Michael Beaton <beatonma@gmail.com>
2019-04-09 20:55:29 +0000

    Added admin for TemporaryMention

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
 
-from .models import MyMentionableArticle
+from .models import MentionableExample, TemporaryMention
 from mentions.models.webmention import Webmention
+
 
 class WebmentionInline(GenericTabularInline):
     model = Webmention
     readonly_fields = ['target_url', 'source_url', 'quote', 'hcard', 'sent_by', ]
     can_delete = False
 
-@admin.register(MyMentionableArticle)
+
+@admin.register(MentionableExample)
 class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     inlines = [
         WebmentionInline,
     ]
+
+@admin.register(TemporaryMention)
+class TemporaryMentionAdmin(admin.ModelAdmin):
+    list_display = ['url', 'submission_time', 'alive']
